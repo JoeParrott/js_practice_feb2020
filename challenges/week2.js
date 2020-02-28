c8566c1755cca755405efe95612e3e1ee850be63
JoeParrott <JosephParrott1@outlook.com>
2020-02-28 03:16:04 +0000

    finished week2 challenges, starting to use fewer lines of code

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,26 +1,38 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  else if (person.city === "Manchester") {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil(people/40)
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  return arr.filter(phrase => phrase === "sheep").length
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (Number.isInteger(person.address.postCode[1]*1)) {
+    if (person.address.postCode[0] === "M") {
+      return true
+    }
+  }
+  else {
+    return false
+  }
 }
 
 module.exports = {
